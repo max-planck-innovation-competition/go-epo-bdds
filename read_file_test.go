9072d5b72d9aa5b191d572e73e0405f929ef14ca
Seb <git@erhardt.net>
2022-12-03 20:25:07 +0100

    rename and add xgen go model

--- read_file_test.go
+++ read_file_test.go
@@ -1,7 +1,27 @@
-package go_epo_docdb
+package go_epo_bdds
 
-import "testing"
+import (
+	"encoding/xml"
+	"github.com/max-planck-innovation-competition/go-epo-docdb/pkg/docdb"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
 
 func TestReadFile(t *testing.T) {
 
+	// process file
+	data, err := ioutil.ReadFile("./pkg/docdb/test-data/AP-1206-A_302101161.xml")
+	if err != nil {
+		t.Error(err)
+	}
+	// replace bytes
+	xmlString := strings.Replace(string(data), "<exch:", "<", -1)
+	xmlString = strings.Replace(xmlString, "</exch:", "</", -1)
+	// parse data from cml in to exchange object
+	var exchangeObject docdb.Exchangedocument
+	err = xml.Unmarshal([]byte(xmlString), &exchangeObject)
+	if err != nil {
+		t.Error(err)
+	}
 }
