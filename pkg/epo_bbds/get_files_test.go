f0f98027c43fda854588cbd3827ab7b98d7170c9
Seb <git@erhardt.net>
2023-03-18 09:03:35 +0100

    total file size print

--- pkg/epo_bbds/get_files_test.go
+++ pkg/epo_bbds/get_files_test.go
@@ -3,6 +3,8 @@ package epo_bbds
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"regexp"
+	"strconv"
 	"testing"
 )
 
@@ -14,13 +16,13 @@ func TestGetDocDbFrontFileLinks(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	resFrontFiles, err := GetEpoBddsFileItems(resToken, EpoDocDBFrontFilesProductID)
+	res, err := GetEpoBddsFileItems(resToken, EpoDocDBFrontFilesProductID)
 	ass.NoError(err)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	fmt.Println(resFrontFiles)
+	printFiles(res)
 }
 
 func TestGetDocDbBackFileLinks(t *testing.T) {
@@ -31,17 +33,58 @@ func TestGetDocDbBackFileLinks(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	resFrontFiles, err := GetEpoBddsFileItems(resToken, EpoDocDBBackFilesProductID)
+	res, err := GetEpoBddsFileItems(resToken, EpoDocDBBackFilesProductID)
 	ass.NoError(err)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	fmt.Println(resFrontFiles)
-	for _, d := range resFrontFiles.Deliveries {
+	printFiles(res)
+}
+
+func printFiles(res EpoProductDeliveriesResponse) {
+	totalFileSize := int64(0)
+	for _, d := range res.Deliveries {
 		fmt.Println(d.DeliveryName)
 		for _, f := range d.Files {
+			size, err := parseFileSize(f.FileSize)
+			if err != nil {
+				fmt.Println(err)
+				break
+			} else {
+				totalFileSize += size
+			}
 			fmt.Println("\t"+f.FileName, f.FileSize)
 		}
 	}
+	fmt.Println("\n\nTotal file size in GB:", float64(totalFileSize)/1024/1024/1024)
+}
+
+func parseFileSize(size string) (result int64, err error) {
+	// e.g.  1.7 GB
+	// extract number and unit
+	re := regexp.MustCompile(`(\d+\.?\d*)\s*(\w+)`)
+	matches := re.FindStringSubmatch(size)
+	if len(matches) != 3 {
+		return 0, fmt.Errorf("could not parse size: %s", size)
+	}
+	// number
+	number, err := strconv.ParseFloat(matches[1], 64)
+	if err != nil {
+		return 0, err
+	}
+	// unit
+	unit := matches[2]
+
+	switch unit {
+	case "GB":
+		result = int64(number * 1024 * 1024 * 1024)
+		return
+	case "MB":
+		result = int64(number * 1024 * 1024)
+		return
+	case "KB":
+		result = int64(number * 1024)
+	}
+	return
 }
